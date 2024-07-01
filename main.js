d6289c238224ef950738ff4af2c20dfd1c611857
ArtemVasiliev <6524652@mail.ru>
2024-07-01 21:22:15 +0300

    feat:added new logic

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const containerFilmsSearched = document.querySelector(".movie--searched");
-const containerFilmsWatched = document.querySelector(".movie--searched");
+const containerFilmsWatched = document.querySelector(".movie--watched");
 const searchForm = document.querySelector(".search__form");
 const searchInput = document.querySelector(".search-input");
 const mask = document.querySelector(".mask");
@@ -15,12 +15,12 @@ const url =
 const urlImage = "https://image.tmdb.org/t/p/w300_and_h450_bestv2";
 
 const state = {
-  films: "",
+  films: [],
   watchedFilms: [],
 };
 
 searchForm.addEventListener("submit", (event) => {
-  containerFilms.innerHTML = "";
+  containerFilmsSearched.innerHTML = "";
   event.preventDefault();
   getFilms(searchInput.value);
 });
@@ -37,11 +37,12 @@ function toggleActiveTabBtn(event) {
     tabBtn.classList.add("active");
   }
   if (activeBtn.classList.contains("tabs__btn--first")) {
-    filmsContainersList[1].classList.add("unvisible");
-    filmsContainersList[0].classList.remove("unvisible");
-  } else if (!activeBtn.classList.contains("tabs__btn--first")) {
     filmsContainersList[0].classList.add("unvisible");
     filmsContainersList[1].classList.remove("unvisible");
+    renderWacthFilms(state.watchedFilms);
+  } else if (!activeBtn.classList.contains("tabs__btn--first")) {
+    filmsContainersList[1].classList.add("unvisible");
+    filmsContainersList[0].classList.remove("unvisible");
   }
 }
 
@@ -54,6 +55,7 @@ function findUnsibileContainer() {
 
 async function getFilms(item) {
   try {
+    console.log("dsfss");
     mask.classList.add("visible");
     const response = await fetch(`${url}${item}`);
     let json = await response.json();
@@ -76,9 +78,43 @@ function renderFilms(item) {
   films.forEach((element) => {
     const film = createMovie(element);
     containerFilmsSearched.append(film);
+    const watchBtn = film.querySelector(".movie__watch-btn");
+    state.films.push(element);
+    film.dataset.id = element.id;
+    watchBtn.addEventListener("click", (event) => {
+      const watchFilm = event.target.closest(".movie");
+      const watchedFilmsId = watchFilm.dataset.id;
+      const watchFilmObj = state.films.find(
+        (obj) => obj.id === parseInt(watchedFilmsId)
+      );
+      if (!state.watchedFilms.some((obj) => obj.id === watchFilmObj.id)) {
+        state.watchedFilms.push(watchFilmObj);
+      }
+    });
   });
 }
-
+function renderWacthFilms(array) {
+  const films = array;
+  containerFilmsWatched.innerHTML = "";
+  films.forEach((element) => {
+    const film = createWatchFilm(element);
+    containerFilmsWatched.append(film);
+    film.dataset.id = element.id;
+  });
+}
+function createWatchFilm(film) {
+  const movie = document.createElement("div");
+  movie.classList.add("movie");
+  movie.innerHTML = `<img src='${urlImage}${film.poster_path}'
+    alt="movie.original_title" class="movie-img" />
+<div>
+    <div class="movie-name">
+        ${film.original_title}
+    </div>
+    <span class="movie-overview">${film.overview}</span>
+</div>`;
+  return movie;
+}
 function createMovie(film) {
   const movie = document.createElement("div");
   movie.classList.add("movie");
@@ -91,12 +127,7 @@ function createMovie(film) {
     <span class="movie-overview">${film.overview}</span>
 </div>
 <button class = "movie__watch-btn">Watch</button>`;
-  const watchBtn = movie.querySelector(".movie__watch-btn");
-  watchBtn.addEventListener("click", () => {
-    state.watchedFilms.push(film);
-    localStorage.setItem("watchedFilms", JSON.stringify(state.watchedFilms));
-    renderFilms(state.watchedFilms, containerFilmsWatched);
-  });
+
   return movie;
 }
 
@@ -105,12 +136,6 @@ document.addEventListener("DOMContentLoaded", () => {
     mask.classList.add("visible");
     const films = JSON.parse(localStorage.getItem("films"));
     renderFilms(films);
-    mask.classList.remove("visible");
-  }
-  if (localStorage.getItem("watchedFilms") !== null) {
-    mask.classList.add("visible");
-    const watchedFilms = JSON.parse(localStorage.getItem("watchedFilms"));
-    renderFilms(watchedFilms);
     mask.classList.remove("visible");
   }
 });
